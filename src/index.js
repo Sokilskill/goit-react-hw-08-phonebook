fb738342f25f447fff15709c60640e4261425c18
Sokilskill <vprovorotov@gmail.com>
2023-10-22 14:10:27 +0300

    chakra

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
+
 import store from 'redux/store';
 import { App } from 'components/App';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor } from 'redux/store';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook">
-        <App />
-      </BrowserRouter>
+      <ChakraProvider>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+          <App />
+        </BrowserRouter>
+      </ChakraProvider>
     </PersistGate>
   </Provider>
   // </React.StrictMode>
