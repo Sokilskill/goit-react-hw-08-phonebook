4b43676c5d876c78211ad35222b587e79f676a64
Sokilskill <vprovorotov@gmail.com>
2023-10-23 11:03:55 +0300

    added Weather Api

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,26 @@
-import { Box, Container, Heading } from '@chakra-ui/react';
+import { Box, Container, Flex, Heading, Text } from '@chakra-ui/react';
+import WeatherForm from 'components/WeatherForm/WeatherForm';
 
 const HomePage = () => {
   return (
     <Box as="section" py={20}>
-      <Container maxW="2xl">
-        <Heading as="h1" size="xl" textAlign="center">
-          Вітаю в додатку Phonebook
-        </Heading>
+      <Container maxW="800px">
+        <Flex gap="20px" justify="space-between">
+          <Box maxW={400}>
+            <Heading as="h1" size={['md', 'lg']} textAlign="center" mb={5}>
+              Your Personal Contact Management Application
+            </Heading>
+            <Text textAlign="center">
+              Hello, this is your PhoneBook. It is designed to help your brain
+              not remember numbers, but write them down. Create your own unique
+              account and get the opportunity to record your contacts.
+            </Text>
+          </Box>
+
+          <Box maxW={500}>
+            <WeatherForm />
+          </Box>
+        </Flex>
       </Container>
     </Box>
   );
---
+++ src/servicApi/wheatherApi.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+const apiKey = 'f00c38e0279b7bc85480c3fe775d518c';
+
+export const getWeather = async query => {
+  const resp = await axios.get(baseUrl, {
+    params: {
+      q: query,
+      units: 'metric',
+      appid: apiKey,
+    },
+  });
+  return resp;
+};
