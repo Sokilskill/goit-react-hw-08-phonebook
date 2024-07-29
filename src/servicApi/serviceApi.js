b31473536fa231d3af36d6696ba28cd7319e671a
Sokilskill <vprovorotov@gmail.com>
2024-07-29 18:02:12 +0300

    feat/ update baseUrl

--- src/servicApi/serviceApi.js
+++ src/servicApi/serviceApi.js
@@ -1,15 +1,7 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
-// const setAuthHeader = token => {
-//   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-// };
-// const clearAuthHeader = () => {
-//   axios.defaults.headers.common.Authorization = '';
-// };
-
 export const instance = axios.create({
-  baseURL: 'https://connections-api.herokuapp.com',
+  baseURL: 'https://connections-api.goit.global/',
 });
 
 const setAuthHeader = token => {
@@ -37,6 +29,7 @@ export const deleteContactById = async id => {
   const { data } = await instance.delete(`/contacts/${id}`);
   return data;
 };
+
 // Authorization
 export const signup = async credentials => {
   const { data } = await instance.post('/users/signup', credentials);
