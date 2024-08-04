9202f416df38b53ed0e2ae2f2edc552b3dff33e5
Sokilskill <vprovorotov@gmail.com>
2024-08-04 22:51:46 +0300

    refactoring/ Extracting Reusable Form Components
    - Extracted `FormInput` component for input fields using `useField` from Formik.
    - Improved the structure of the login form, reduced code duplication.
    - Enhanced code flexibility and cleanliness.

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,21 +1,9 @@
-import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import * as Yup from 'yup';
-import { Formik, Form, Field } from 'formik';
-import {
-  FormControl,
-  FormLabel,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Button,
-  Text,
-  Flex,
-  Icon,
-  FormErrorMessage,
-} from '@chakra-ui/react';
-import { MdVisibility, MdVisibilityOff, MdCheckCircle } from 'react-icons/md';
+import { Formik, Form } from 'formik';
+import { Button, Flex } from '@chakra-ui/react';
 import { logIn } from 'redux/auth/operations';
+import FormInput from 'components/FormInput/FormInput';
 
 const initialValues = {
   name: '',
@@ -29,12 +17,10 @@ const SignUpSchema = Yup.object().shape({
     .required('Email is required'),
   password: Yup.string()
     .required('Password is required')
-    .min(6, 'Password is too short - should be 6 chars minimum'),
+    .min(5, 'Password is too short - should be 5 chars minimum'),
 });
 
 const LoginForm = () => {
-  const [visible, setVisible] = useState(false);
-
   const dispatch = useDispatch();
 
   return (
@@ -47,7 +33,7 @@ const LoginForm = () => {
         resetForm();
       }}
     >
-      {({ errors, touched, values }) => (
+      {() => (
         <Flex
           as={Form}
           direction="column"
@@ -61,55 +47,19 @@ const LoginForm = () => {
           borderRadius="md"
           autoComplete="new-password"
         >
-          <FormControl isInvalid={errors.email && touched.email}>
-            <FormLabel htmlFor="email">Email</FormLabel>
-            <Field
-              as={Input}
-              name="email"
-              id="email"
-              type="email"
-              placeholder="Email"
-              _placeholder={{ color: 'black' }}
-            />
-            <FormErrorMessage>
-              <Text color="red.500">{errors.email}</Text>
-            </FormErrorMessage>
+          <FormInput
+            name="email"
+            type="email"
+            label="Email"
+            placeholder="Enter email"
+          />
 
-            {!errors.email && values.email && (
-              <Text color="green.500">
-                <Icon as={MdCheckCircle} />
-                Success email
-              </Text>
-            )}
-          </FormControl>
-
-          <FormControl isInvalid={errors.password && touched.password}>
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <InputGroup>
-              <Field
-                as={Input}
-                name="password"
-                id="password"
-                type={visible ? 'text' : 'password'}
-                placeholder="Password"
-                _placeholder={{ color: 'black' }}
-                autoComplete="new-password"
-              />
-              <InputRightElement>
-                <Button variant="ghost" onClick={() => setVisible(!visible)}>
-                  <Icon as={visible ? MdVisibility : MdVisibilityOff} />
-                </Button>
-              </InputRightElement>
-            </InputGroup>
-            <FormErrorMessage>{errors.password}</FormErrorMessage>
-
-            {!errors.password && values.password && (
-              <Text color="green.500">
-                <Icon as={MdCheckCircle} />
-                Success password
-              </Text>
-            )}
-          </FormControl>
+          <FormInput
+            name="password"
+            type="password"
+            label="Password"
+            placeholder="Enter password"
+          />
 
           <Button type="submit" colorScheme="teal" mt="4">
             Sign In
