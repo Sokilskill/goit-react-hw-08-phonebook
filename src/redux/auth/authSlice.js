cd2f8be0b6cf45ac14d14334849325a30515044a
Sokilskill <vprovorotov@gmail.com>
2024-08-05 18:03:30 +0300

    feat(auth)/ implements error handling for HTTP 400
    - intercepts 400 errors within async thunk and propagates error details using thunkAPI.rejectWithValue
    - updates the slice state by setting the error message to the state.error property
    - defines a selector to extract the error message from the state

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -6,6 +6,7 @@ const initialState = {
   token: null,
   isLoggedIn: false,
   isRefreshing: false,
+  isLoading: false,
   error: null,
 };
 
@@ -16,35 +17,59 @@ const authSlice = createSlice({
     builder
       .addCase(register.pending, state => {
         state.error = null;
+        state.isLoading = true;
       })
       .addCase(register.fulfilled, (state, { payload }) => {
         state.user = payload.user;
         state.token = payload.token;
         state.isLoggedIn = true;
+        state.isLoading = false;
       })
       .addCase(register.rejected, (state, { payload }) => {
         state.error = payload;
+        state.isLoading = false;
+      })
+      .addCase(logIn.pending, state => {
+        state.error = null;
+        state.isLoading = true;
       })
       .addCase(logIn.fulfilled, (state, { payload }) => {
         state.user = payload.user;
         state.token = payload.token;
         state.isLoggedIn = true;
+        state.isLoading = false;
+      })
+      .addCase(logIn.rejected, (state, { payload }) => {
+        state.error = payload;
+        state.isLoading = false;
+      })
+      .addCase(logOut.pending, state => {
+        state.isLoading = true;
       })
       .addCase(logOut.fulfilled, state => {
         state.user = { name: null, email: null };
         state.token = null;
         state.isLoggedIn = false;
+        state.error = null;
+        state.isLoading = false;
+      })
+      .addCase(logOut.rejected, (state, { payload }) => {
+        state.error = payload;
+        state.isLoading = false;
       })
       .addCase(refreshUser.pending, state => {
         state.isRefreshing = true;
+        state.isLoading = true;
       })
       .addCase(refreshUser.fulfilled, (state, { payload }) => {
         state.user = payload;
         state.isLoggedIn = true;
         state.isRefreshing = false;
+        state.isLoading = false;
       })
       .addCase(refreshUser.rejected, state => {
         state.isRefreshing = false;
+        state.isLoading = false;
       }),
 });
 
