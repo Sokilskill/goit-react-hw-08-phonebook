75714232f7e986074917e8aba656cb2279ea7f85
Sokilskill <vprovorotov@gmail.com>
2024-08-07 21:31:42 +0300

    Refactor/ Create UniversalForm component and move Yup schemas
    - Created UniversalForm component to handle various form operations.
    - Moved Yup schemas to a separate file in `yupSchemas/index.js` for better organization.
    - Added toast notifications for form submission errors in UniversalForm.
    - Updated LoginForm and RegisterForm to use UniversalForm and referenced schema from `yupSchemas/index.js`.
    
    Changes include:
    - UniversalForm component with default and custom handleSubmit functions.
    - LoginForm and RegisterForm now imports and uses UniversalForm.
    - Updated error handling with toast notifications.
    - Schema definitions relocated to `yupSchemas/index.js`.

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,91 +1,35 @@
-import { useDispatch } from 'react-redux';
-import * as Yup from 'yup';
-import { Formik, Form } from 'formik';
-import { Button, Flex } from '@chakra-ui/react';
 import { logIn } from 'redux/auth/operations';
 import FormInput from 'components/FormInput/FormInput';
+import UniversalForm from 'components/UniversalForm/UniversalForm';
+import { LogInSchema } from 'yupSchemas';
 
 const initialValues = {
-  name: '',
   email: '',
   password: '',
 };
 
-const SignUpSchema = Yup.object().shape({
-  email: Yup.string()
-    .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, 'Is not in correct format')
-    .required('Email is required'),
-  password: Yup.string()
-    .required('Password is required')
-    .min(5, 'Password is too short - should be 5 chars minimum'),
-});
-
 const LoginForm = () => {
-  const dispatch = useDispatch();
-
   return (
-    <Formik
+    <UniversalForm
       initialValues={initialValues}
-      validationSchema={SignUpSchema}
-      validateOnChange
-      onSubmit={(values, { resetForm, setSubmitting }) => {
-        dispatch(logIn(values)).then(() => {
-          setSubmitting(false);
-          resetForm();
-        });
-      }}
-      // onSubmit={async (values, { resetForm, setSubmitting }) => {
-      //   try {
-      //     const result = await dispatch(logIn(values));
-      //     if (result) {
-      //       resetForm();
-      //     }
-      //   } finally {
-      //     setSubmitting(false);
-      //   }
-      // }}
+      validationSchema={LogInSchema}
+      operation={logIn}
+      labelButtonSbmit="Log in"
     >
-      {props => {
-        return (
-          <Flex
-            as={Form}
-            direction="column"
-            gap="10px"
-            maxW="md"
-            mx="auto"
-            my="4"
-            p="4"
-            border="1px solid"
-            borderColor="gray.300"
-            borderRadius="md"
-            autoComplete="new-password"
-          >
-            <FormInput
-              name="email"
-              type="email"
-              label="Email"
-              placeholder="Enter email"
-            />
+      <FormInput
+        name="email"
+        type="email"
+        label="Email"
+        placeholder="Enter email"
+      />
 
-            <FormInput
-              name="password"
-              type="password"
-              label="Password"
-              placeholder="Enter password"
-            />
-
-            <Button
-              type="submit"
-              colorScheme="teal"
-              mt="4"
-              isLoading={props.isSubmitting}
-            >
-              Sign In
-            </Button>
-          </Flex>
-        );
-      }}
-    </Formik>
+      <FormInput
+        name="password"
+        type="password"
+        label="Password"
+        placeholder="Enter password"
+      />
+    </UniversalForm>
   );
 };
 
--- src/components/RegistrForm/RegisterForm.jsx
+++ src/components/RegistrForm/RegisterForm.jsx
@@ -1,9 +1,7 @@
-import { useDispatch } from 'react-redux';
-import { Formik, Form } from 'formik';
-import * as Yup from 'yup';
-import { Button, Flex } from '@chakra-ui/react';
 import { register } from 'redux/auth/operations';
 import FormInput from 'components/FormInput/FormInput';
+import UniversalForm from 'components/UniversalForm/UniversalForm';
+import { SignUpSchema } from 'yupSchemas';
 
 const initialValues = {
   name: '',
@@ -11,74 +9,29 @@ const initialValues = {
   password: '',
 };
 
-const SignUpSchema = Yup.object().shape({
-  name: Yup.string()
-    .min(3, 'Too Short!')
-    .matches(/^[A-Za-z]+$/, 'Only English letters')
-    .required('Required'),
-  email: Yup.string()
-    .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, 'Is not in correct format')
-    .required('Email is required'),
-  password: Yup.string()
-    .required('Password is required')
-    .min(7, 'Password is too short - should be 7 chars minimum'),
-});
-
 const RegisterForm = () => {
-  const dispatch = useDispatch();
-
   return (
-    <Formik
+    <UniversalForm
       initialValues={initialValues}
       validationSchema={SignUpSchema}
-      validateOnChange
-      onSubmit={(values, { resetForm, setSubmitting }) => {
-        dispatch(register(values)).then(() => {
-          setSubmitting(false);
-          resetForm();
-        });
-      }}
+      operation={register}
+      labelButtonSbmit="Sign up"
     >
-      {({ isSubmitting }) => (
-        <Flex
-          as={Form}
-          direction="column"
-          gap="10px"
-          maxW="md"
-          mx="auto"
-          my="4"
-          p="4"
-          border="1px solid"
-          borderColor="gray.300"
-          borderRadius="md"
-          autoComplete="new-password"
-        >
-          <FormInput name="name" label="Name" placeholder="Enter nick-name" />
-          <FormInput
-            name="email"
-            type="email"
-            label="Email"
-            placeholder="Enter email"
-          />
+      <FormInput name="name" label="Name" placeholder="Enter nick-name" />
+      <FormInput
+        name="email"
+        type="email"
+        label="Email"
+        placeholder="Enter email"
+      />
 
-          <FormInput
-            name="password"
-            type="password"
-            label="Password"
-            placeholder="Enter password"
-          />
-
-          <Button
-            type="submit"
-            colorScheme="teal"
-            mt="4"
-            isLoading={isSubmitting}
-          >
-            Sign Up
-          </Button>
-        </Flex>
-      )}
-    </Formik>
+      <FormInput
+        name="password"
+        type="password"
+        label="Password"
+        placeholder="Enter password"
+      />
+    </UniversalForm>
   );
 };
 
